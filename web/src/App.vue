4dd95f21cd09383df3c86f3175b6ee4a61a1ec4f
Alex X <iteratorx@zoho.eu>
2018-03-03 13:53:04 +0200

    added dropzone upload to front-end

--- web/src/App.vue
+++ web/src/App.vue
@@ -21,24 +21,61 @@
       audioSrc: {{ audioSrc }}
       </p>
     </div>
+    <div class="container">
+      <vue-dropzone ref="myVueDropzone" id="dropzone" :options="dropzoneOptions" v-on:vdropzone-success="afterUploadOk">
+      </vue-dropzone>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import vue2Dropzone from 'vue2-dropzone'
+import 'vue2-dropzone/dist/vue2Dropzone.css'
 
 export default {
   name: "app",
+  components: {
+    vueDropzone: vue2Dropzone
+  },
   data() {
     return {
       imageSrc: "",
       audioSrc: "",
-      sound_file_id: ""
+      sound_file_id: "",
+      dropzoneOptions: {
+          // url: '/api/v1/spectrogram',
+          // url: 'http://localhost:5000/api/v1/spectrogram',
+          url: '/api/v1/spectrogram',
+          method: 'post',
+          thumbnailWidth: 150,
+          maxFilesize: 50,
+          headers: { 
+            // "Content-Type": "multipart/form-data",
+            Accept: "application/json, text/plain, */*",
+            'Access-Control-Allow-Origin': "*" 
+          },
+          paramName: 'sound_file'
+      },
+      debug: false
 
     };
   },
 
+
   methods: {
+
+    afterUploadOk: function(file,response) {
+        console.log("POST request completed:")
+        console.log("got response: " + JSON.stringify(response));
+        console.log("sound_file_id=" + response.file_id)
+        
+        this.sound_file_id = response.file_id;
+        this.imageSrc = "/api/v1/spectrogram?file_id=" + this.sound_file_id
+        this.audioSrc = "/uploads/" + this.sound_file_id + ".wav"
+        console.log("imageSrc: " + this.imageSrc)
+    },
+
     uploadImage: function(e) {
       var files = e.target.files;
       if (!files[0]) {
